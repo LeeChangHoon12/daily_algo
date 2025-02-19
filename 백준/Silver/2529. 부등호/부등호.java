6bc082884a6089cb871ab64c7720c4bd002831ac
LeeChangHoon12 <53554944+LeeChangHoon12@users.noreply.github.com>
2025-02-20 00:29:59 +0900

    [Silver I] Title: 부등호, Time: 452 ms, Memory: 22356 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -4,12 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 /**
+ * 메모리 : 23712kb	시간 : 452ms
+ * 체감난이도 : 5/10
+ * 
+ * 어려웠던 점 : 배열 복사할 때, 자꾸 깊은 복사 얕은 복사 실수를 함. 그래도 예전에는 실수자체를 인지못했지만,
+ *              이번에는 바로 확인하고, 배열 복사부터 확인해서 수정할 수 있었음.
+ *               !!!! 1차 역량테스트 보충시간에 배웠던  System.arraycopy() 메소드를 사용해봄 !!!!!
+ * 
  * 순서열 A : < 혹은 > 로 k개가 나열
  * 부등호 앞 뒤에 숫자를 넣어서 모든 부등호 관계가 만족해야함.
  * ex) 3 < 4 < 5 < 6 > 1 < 2 < 8 > 7 < 9 > 0
  * 여기서 부등호를 제거하면, 부등호 관계를 만족하는 정수 = 3456128790
  * 위 만족하는 정수의 최대값과 최솟값 찾기
  *
+ * 아이디어 : 
+ * 숫자의 갯수 = K+1개가 됨
+ * K+1개 숫자로 만들 수 있는 순열을 모두 부등호에 대입해서 확인
+ * 성공한 숫자로 최대값 최소값 갱신
+ * 
+ * 최대값과 출력을 위한 최대값을 만드는 숫자배열을 따로 관리함(맨 앞에 0이 들어가는 경우에 0도 출력해줘야하는데, 배열로 하면 맨앞에 0부터 출력할 수 있음)
+ * 
  *
  * */
 public class Main {
@@ -67,16 +81,12 @@ public class Main {
                 //System.out.println(nums);
                 if(nums > max ){
                     max = nums;
-                    for(int i=0; i<targetNums.length; i++){
-                        maxArr[i] = targetNums[i];
-                    }
+                    System.arraycopy(targetNums, 0, maxArr, 0, targetNums.length);
                 }
 
                 if(nums < min){
                     min = nums;
-                    for(int i=0; i<targetNums.length; i++){
-                        minArr[i] = targetNums[i];
-                    }
+                    System.arraycopy(targetNums, 0, minArr, 0, targetNums.length);
                 }
 
             }
