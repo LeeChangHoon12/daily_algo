91229a24b4c9903eb4b1abbee57d91a6d818aa94
LeeChangHoon12 <53554944+LeeChangHoon12@users.noreply.github.com>
2025-03-09 02:29:29 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1100 ms, Memory: 190800 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,75 +1,31 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int[] arr;
-	static int N;
-	static int M;
-	static int[] cntPlus = new int[10000001];
-	static int[] cntMinus = new int[10000001];;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		for(int i=0; i<N; i++) {
-			int target = Integer.parseInt(st.nextToken());
-			arr[i] = target;
-		}
+        int N = Integer.parseInt(br.readLine());
+        HashMap<Integer,Integer> m = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            if(m.containsKey(num)){
+                m.put(num, m.get(num)+1);
+            }else{
+                m.put(num,1);
+            }
+        }
 
-		Arrays.sort(arr);
-		
-		M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<M; i++) {
-			int target = Integer.parseInt(st.nextToken());
-			
-			sb.append(upperbound(target) - lowerbound(target) + " ");
-		}
-		
-		System.out.println(sb);
-		
-	}
-	
-	public static int lowerbound(int target) {
-	
-		int st = 0;
-		int en = N;
-		
-		while(st < en) {
-			int mid =(st+en)/2;
-			if(arr[mid] >= target) {
-				en = mid;
-			}else {
-				st = mid+1;
-			}
-		}
-		return st;
-	}
-	
-	public static int upperbound(int target) {
-		
-		int st = 0;
-		int en = N;
-		
-		while(st < en) {
-			int mid = (st+en)/2;
-			if(arr[mid] > target) {
-				en = mid;
-			}else{				
-				st = mid+1;
-			}
-		}
-		
-		return st;
-	}
-	
-
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb  = new StringBuilder();
+        for(int i=0; i<M; i++){
+            //System.out.print(m.getOrDefault(Integer.parseInt(st.nextToken()),0)+ " ");
+            sb.append(m.getOrDefault(Integer.parseInt(st.nextToken()),0)+ " ");
+        }
+        System.out.print(sb);
+    }
 }
