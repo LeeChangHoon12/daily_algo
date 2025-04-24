1f449d38f081024ecdba3e30b600f4f48aa23452
LeeChangHoon12 <53554944+LeeChangHoon12@users.noreply.github.com>
2025-04-24 13:40:54 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 22.54 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        StringTokenizer st = new StringTokenizer(s);
+        while(st.hasMoreTokens()){
+            int num = Integer.parseInt(st.nextToken());
+            max = Math.max(max,num);
+            min = Math.min(min,num);
+        }
+        
+        return  min + " " + max;
+    }
+}
